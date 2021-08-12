b6f7a1f5be83e57de94c013c43969a0f0d333b78
Dan <DanielMooreRealPerson@gmail.com>
2021-08-11 22:28:00 -0400

    commit tree f28c6a8d3c6d15acf4fb6cf32f5406d5f52b55c3

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+function mapToNegativize(arr) {
+    let x = []
+    let y;
+    for ( y of arr) {
+        x.push(y * -1)
+    }
+    return x
+}
+
+function mapToNoChange(arr) {
+    return arr
+}
+
+function mapToDouble(arr) {
+    let x = []
+    let y;
+    for ( y of arr) {
+        x.push(y * 2)
+    }
+    return x
+}
+
+function mapToSquare(arr) {
+    let x = []
+    let y;
+    for ( y of arr) {
+        x.push(y ** 2)
+    }
+    return x
+}
+
+function reduceToTotal(arr, startingPoint=0) {
+    let total = startingPoint
+    let y;
+    for ( y of arr) {
+        total += y
+    }
+    return total
+}
+
+function reduceToAllTrue(arr) {
+    let y;
+    for (y of arr) {
+        if (!y)
+        return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    let y;
+    for (y of arr) {
+        if (y)
+        return true
+    }
+    return false
+}
